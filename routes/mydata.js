53d6fdad6eb257d01275a48396525b87664b3c85
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-10-27 18:00:35 -0500

    New changes

---
+++ routes/mydata.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/mydata', function(req, res, next) {
+  res.render('mydata', { title: 'Manish Goud Bandharapu' });
+});
+module.exports = router;
