7ad1ed331f867a2d632bdb21cb190c2df7388780
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-10-27 20:49:05 -0500

    changes not done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var radi = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  radi = radi + 1;
+  res.send(`User accesses are: ${radi}`);
 });
 
 module.exports = router;
